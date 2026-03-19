9eaf5add8c2da5f09c65f79dc69977c257066157
yicheng <l5977951@gmail.com>
2026-03-19 21:51:50 +0800

    feat(ptp): improve timestamp logging in packet reception - Refactor timestamp logging for better readability - Ensure consistent formatting of log messages
    This enhances the clarity of received timestamp logs.

--- examples/ethernet/ptp/components/ptpd/ptpd.c
+++ examples/ethernet/ptp/components/ptpd/ptpd.c
@@ -380,10 +380,10 @@ static int ptp_net_recv(FAR struct ptp_state_s *state, void *ptp_msg, uint16_t p
 
   // check if read was successful, ts exists and ts_info is valid
   if (ret > 0 && ts && ts_info->type == L2TAP_IREC_TIME_STAMP)
-    {
-      *ts = *(struct timespec *)ts_info->data;
-      ESP_LOGI(TAG, "RX ts: %lld.%09ld", (long long)ts->tv_sec, ts->tv_nsec);
-    }
+  {
+    *ts = *(struct timespec *)ts_info->data;
+    ESP_LOGI(TAG, "RX ts: %lld.%09ld", (long long)ts->tv_sec, ts->tv_nsec);
+  }
 
   memcpy(ptp_msg, &eth_frame[ETH_HEADER_LEN], ret);
 
