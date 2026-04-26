0b7fdc68876582bad2de7e916ca468d3b4a016d2
yicheng <l5977951@gmail.com>
2026-04-26 17:42:37 +0800

    feat(esp_eth): enhance DM9058 support and PTP integration - Reorganize source files for DM9058 Ethernet controller - Update Kconfig to improve DM9058 driver configuration - Refactor PTP time structure for better compatibility This enhances the functionality and configurability of the DM9058 driver.

--- components/esp_eth/src/spi/dm9058/esp_eth_ptp_dm9058.h
+++ components/esp_eth/src/spi/dm9058/esp_eth_ptp_dm9058.h
@@ -4,16 +4,13 @@
 #include <stddef.h>
 #include <stdint.h>
 #include "esp_err.h"
-#include "esp_eth_mac_spi.h"  // for esp_eth_ptp_dm9058_transport_t, esp_eth_ptp_dm9058_enable_config_t
+#include "esp_eth_mac_spi.h"  // for eth_dm9058_ptp_time_t
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-typedef struct {
-    uint32_t seconds;
-    uint32_t nanoseconds;
-} esp_eth_ptp_dm9058_time_t;
+typedef eth_dm9058_ptp_time_t esp_eth_ptp_dm9058_time_t;
 
 typedef struct {
     esp_err_t (*reg_read)(void *io_ctx, uint8_t reg, uint8_t *value);
@@ -62,7 +59,7 @@ typedef struct {
 } esp_eth_ptp_dm9058_rx_info_t;
 
 esp_err_t esp_eth_ptp_dm9058_init(esp_eth_ptp_dm9058_t *ptp, void *io_ctx, const esp_eth_ptp_dm9058_ops_t *ops);
-esp_err_t esp_eth_ptp_dm9058_enable(esp_eth_ptp_dm9058_t *ptp, bool enable, esp_eth_ptp_dm9058_transport_t transport);
+esp_err_t esp_eth_ptp_dm9058_enable(esp_eth_ptp_dm9058_t *ptp, bool enable);
 esp_err_t esp_eth_ptp_dm9058_get_time(esp_eth_ptp_dm9058_t *ptp, esp_eth_ptp_dm9058_time_t *time);
 esp_err_t esp_eth_ptp_dm9058_set_time(esp_eth_ptp_dm9058_t *ptp, const esp_eth_ptp_dm9058_time_t *time);
 esp_err_t esp_eth_ptp_dm9058_adj_time(esp_eth_ptp_dm9058_t *ptp, const esp_eth_ptp_dm9058_time_t *offset);
